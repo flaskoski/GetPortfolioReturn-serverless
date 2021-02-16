4857c463b4ca432c04802b101ffd9abb40cc68e4
flaskoski <felipeflaskoski@outlook.com>
2021-02-16 13:49:10 -0300

    daily returns and avg price completed.

--- getAssetReturn.js
+++ getAssetReturn.js
@@ -1,38 +1,192 @@
 const https = require('https')
-const AWS = require('aws-sdk');
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-exports.main =  function(event, context, callback) {
-  const API_KEY = process.env.ALPHA_API_KEY
-  var outputSize = "compact"
-
-  for(let key in event)
-    console.info(`event[${key}]: ${event[key]}`);
-
-  const code = event["code"];
-  if(!code || !API_KEY){
-    console.error("code/API key not informed!");
-    callback(null, quote);
-  }
-
-  let url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&outputsize="+ outputSize +"&symbol="+ code +".SA&apikey="+ API_KEY
-  var quote = {}
-  
-  https.get(url, (res) => {
-    var body = '';
-    res.on('data', function(chunk){
-        body += chunk;
-    });
-    res.on('end', function(){
-        quote = JSON.parse(body);
-        console.log("Got a response: ", quote['Time Series (Daily)']['2021-02-12']);
-        callback(null, quote['Time Series (Daily)'])
-    });  
-  }).on('error', (e) => {
-    callback(Error(e))
-  })
+// const AWS = require('aws-sdk');
+var dateFormat = require("dateformat");
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const event = {
+    code: "VALE3",
+    startDate: "2020-12-20",
+    endDate: "2021-1-20",
+    transactions:  [
+        {
+            "date": [
+                2020,
+                12,
+                30
+            ],
+            "asset": "VALE3",
+            "shares_number": 100,
+            "price": 85.0,
+            "type": "BUY"
+        },
+        {
+            "date": [
+                2020,
+                12,
+                29
+            ],
+            "asset": "VALE3",
+            "shares_number": 100,
+            "price": 60.2,
+            "type": "BUY"
+        },
+        {
+            "date": [
+                2020,
+                11,
+                20
+            ],
+            "asset": "VALE3",
+            "shares_number": 100,
+            "price": 100.0,
+            "type": "BUY"
+        },
+        {
+            "date": [
+                2020,
+                10,
+                10
+            ],
+            "asset": "VALE3",
+            "shares_number": 100,
+            "price": 90.0,
+            "type": "BUY"
+        },
+        {
+            "date": [
+                2020,
+                9,
+                27
+            ],
+            "asset": "PETR4",
+            "shares_number": 100,
+            "price": 21.0,
+            "type": "BUY"
+        }
+    ]
+}
+// Helper function used to validate input
+function checkDefined(reference, referenceName) {
+    if (!reference) {
+        throw new Error(`Error: ${referenceName} is not defined`);
+    }
+    return reference;
 }
 
+const code = event["code"];
+checkDefined(code, "code");
+
+//exports.main =  function(event, context, callback) {
+function main(event, context, callback) {
+    const API_KEY = process.env.ALPHA_API_KEY
+    var outputSize = "compact"
+    
+    for(let key in event)
+    console.info(`event[${key}]: ${event[key]}`);
+    
+    
+    const transactions = event["transactions"];
+    const startDate = new Date(event["startDate"] + " 15:00");
+    console.log(startDate, event["startDate"]);
+    const endDate = new Date(event["endDate"] + " 15:00");
+    checkDefined(transactions, "transactions");
+    checkDefined(startDate, "startDate");
+    checkDefined(endDate, "endDate");
+    
+    
+    let url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&outputsize="+ outputSize +"&symbol="+ code +".SA&apikey="+ API_KEY
+    var quote = {}
+    
+    https.get(url, (res) => {
+        var body = '';
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+        res.on('end', function(){
+            quotes = JSON.parse(body)['Time Series (Daily)'];
+            console.log("Got a response: ", quotes['2021-02-12']);
+            
+            calculateDailyReturns(transactions, startDate, endDate, quotes)
+            
+            // callback(null, quotes)
+        });  
+    }).on('error', (e) => {
+        callback(Error(e))
+    })
+}
+
+function getTransactionsFromDay(transactions, date, beforeDateToo){
+    return transactions.filter(t => t.asset == code && 
+        (beforeDateToo? toDate(t.date).getTime() <= date.getTime() : toDate(t.date).getTime() == date.getTime()) );
+}
+
+function sumTransactionsFromDay(totals, transactions, currentQuote, date, beforeDateToo){
+    let transactionsFromDay = getTransactionsFromDay(transactions, date, beforeDateToo)
+    transactionsFromDay.forEach(t =>{
+        totals.shares += t.shares_number;
+        totals.cost += t.price * t.shares_number;
+    })
+    if(totals.shares > 0){
+        totals.return = currentQuote / (totals.cost/totals.shares);
+        console.log("newTransactions avg cost:",totals.cost/totals.shares);
+        console.log("newTransactions return:", totals.return);
+    }
+    return transactionsFromDay;
+}
+
+function calculateDailyReturns(transactions, startDate, endDate, quotes){
+    //--skip weekend/holidays
+    for(let day = startDate; day <= endDate; day.setDate(day.getDate() + 1))
+        if(!quotes[dateToString(day).toString()])
+            startDate = day;
+        else break;
+
+    lastDayValue = quotes[dateToString(startDate)]["5. adjusted close"];
+
+    //--get totals from all transactions made before and on start date
+    let totals = {shares: 0, cost: 0, return: 1};
+    sumTransactionsFromDay(totals, transactions, lastDayValue, startDate, true)
+    
+
+    var assetDailyValues = {};
+    for(let day = startDate; day <= endDate; day.setDate(day.getDate() + 1)){
+        //--if its weekend/holiday
+        if(!quotes[dateToString(day).toString()])
+            continue;
+
+        //--get the closed quote of the day
+        todayValue = quotes[dateToString(day).toString()]["5. adjusted close"];
+        // console.log("-----DAY:", day, "------ daily close:", todayValue);
+
+        //--get transactions made on the day
+        let newTransactionsTotals = {shares: 0, cost: 0, return: 1}
+        let transactionsToday = sumTransactionsFromDay(newTransactionsTotals, transactions, todayValue, day, false)
+        
+        //--get totals considering the new day
+        let todaysReturn = todayValue/lastDayValue;
+        totals.return = ( (totals.return*totals.shares) * todaysReturn  
+                                +  newTransactionsTotals.shares*newTransactionsTotals.return )
+                /  (totals.shares + newTransactionsTotals.shares);
+        totals.shares += newTransactionsTotals.shares;
+        totals.cost += newTransactionsTotals.cost;
+
+        assetDailyValues[dateToString(day)] = totals;
+        // console.log("Cumulative return:", totals.return);
+        // console.log("Total shares:", totals.shares);
+        
+        lastDayValue = todayValue;
+    }
+}
+
+function toDate(d){
+    return new Date(`${d[0]}-${d[1]}-${d[2]} 15:00`);
+}
+function dateToString(d){
+    d.setHours(15);
+    return dateFormat(d, "yyyy-mm-dd")
+}
+
+
+main(event)
 
 // 'use strict';
 
@@ -49,7 +203,7 @@ exports.main =  function(event, context, callback) {
 //   var body = '';
 //   //http.get('http://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&outputsize='+outputSize+'&symbol='+code+'.SA&apikey='+API_KEY, function(res){
 //     http.get('http://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&outputsize=compact&symbol=PETR4.SA&apikey=OC8AZHP6EZ0ABFIW', function(res){
-      
+
 //       res.on('data', function(chunk){
 //         body += chunk;
 //       });
@@ -69,7 +223,7 @@ exports.main =  function(event, context, callback) {
 //           ),
 //         };
 //   });
-  
+
 //   function getData(chunk){
 //     var quote = JSON.parse(body);
 //     console.log("Got a response: ", quote);
@@ -88,8 +242,8 @@ exports.main =  function(event, context, callback) {
 //     };
 //   }
 
-  
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+// Use this code if you don't use the http event with the LAMBDA-PROXY integration
+// return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 // };
